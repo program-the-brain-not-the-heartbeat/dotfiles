438860df5bc0c0093801738d960df7225b39f7e3
Matt Lewis <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2025-12-05 09:09:14 -0400

    feat: record spaces too

--- common/bashrc.d/05-history.bash
+++ common/bashrc.d/05-history.bash
@@ -1,6 +1,6 @@
 # don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
-HISTCONTROL=ignoredups:ignorespace
+HISTCONTROL=ignoredups
 
 # append to the history file, don't overwrite it
 shopt -s histappend
@@ -10,3 +10,9 @@ HISTSIZE=-1
 HISTFILESIZE=-1
 
 export HISTTIMEFORMAT="%F %T "
+
+# function historymerge {
+#     history -n; history -w; history -c; history -r;
+# }
+# trap historymerge EXIT
+# PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
