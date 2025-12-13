522172333ba4f2c10674472b188a6e3fe6612442
Matt Lewis <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2025-12-13 13:27:53 -0400

    fix: remove unnecessary

--- common/bashrc.d/45-completion.bash
+++ common/bashrc.d/45-completion.bash
@@ -5,16 +5,8 @@ if [ -d "$HOME/.bash_completion.d" ]; then
 fi
 
 
-if [[ -n "$NVM_DIR" ]]; then
+if [[ -n "${NVM_DIR:-}" ]]; then
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 #restic generate --bash-completion ~/.bash_completion.d/restic
-
-
-
-# This is probably not needed below
-# >>> b2 autocomplete >>>
-# This section is managed by b2 . Manual edit may break automated updates.
-source /root/.bash_completion.d/b2 || true
-# <<< b2 autocomplete <<<
