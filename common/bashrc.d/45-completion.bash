91821b9a4c90cd2d62f944cacfee19155f0552a8
Matt Lewis <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2025-12-13 13:18:55 -0400

    chorce: cleanup bash completion

--- common/bashrc.d/45-completion.bash
+++ common/bashrc.d/45-completion.bash
@@ -1,16 +1,18 @@
+if [ -d "$HOME/.bash_completion.d" ]; then
+  for f in "$HOME/.bash_completion.d/"*; do
+    [ -r "$f" ] && source "$f" || true
+  done
+fi
 
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-#if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-#    . /etc/bash_completion
-#fi
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+#restic generate --bash-completion ~/.bash_completion.d/restic
+
+
+
+# This is probably not needed below
 # >>> b2 autocomplete >>>
 # This section is managed by b2 . Manual edit may break automated updates.
 source /root/.bash_completion.d/b2 || true
 # <<< b2 autocomplete <<<
-
-
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
